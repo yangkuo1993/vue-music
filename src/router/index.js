3ee4a6e943795173bd57d9413638af26e0f99397
yangkuo <734271234@qq.com>
2017-06-29 11:01:02 +0800

    路由实现懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home'
-
 Vue.use(Router)
-
+// 路由采用webpack require.ensure 实现懒加载
+const Home = resolve => {
+  require.ensure(['@/views/Home'], () => {
+    resolve(require('@/views/Home'))
+  })
+} // Home主页
 export default new Router({
   routes: [
     {
