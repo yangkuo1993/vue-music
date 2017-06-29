75fc4012dd2eaea7d10759d62e80af626bfc63e9
yangkuo <734271234@qq.com>
2017-06-29 10:51:30 +0800

    filter,directive

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      home
+      <Hello></Hello>
+    </div>
+</template>
+
+<script>
+  import hello from '@/components/hello'
+  export default {
+    name: 'home',
+    data () {
+      return {}
+    },
+    mounted () {
+      console.log(this.$store.state.user.hotMusicList)
+    },
+    components: {
+      Hello: hello
+    }
+  }
+</script>
+<style>
+
+</style>
