74d52ede764e4d2422a96098661e5929802a343c
杨阔 <734271234@qq.com>
2017-10-30 22:45:39 +0800

    logo

---
+++ src/components/Tab.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tab">
+    <van-tabs>
+        <van-tab title="推荐音乐">推荐音乐</van-tab>
+        <van-tab title="热歌榜">热歌榜</van-tab>
+        <van-tab title="搜索">搜索</van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tab',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 import http from './config/http'
+import { Tab, Tabs } from 'vant'
+Vue.component(Tab.name, Tab)
+Vue.component(Tabs.name, Tabs)
 Vue.config.productionTip = false
 Vue.prototype.$http = http
 /* eslint-disable no-new */
