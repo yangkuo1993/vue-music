f8710416d5691f589a1d026e764e3518766d72e2
yangkuo <734271234@qq.com>
2017-10-30 16:43:29 +0800

    vuex  vant

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/views/home'
 Vue.use(Router)
 
 export default new Router({
@@ -8,7 +8,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: HelloWorld
+      component: Home
     }
   ]
 })
---
+++ src/views/home.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <ul @click="clickUl">
+            <li v-for="item in list" :key="item.id">{{item.name}}</li>
+        </ul>
+        <div class="home" @click.stop="clickOne">
+            <div class="one" @click.stop="clickTwo">
+                <div class="two" @click.stop="clickThree">inner</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      list: [
+        {
+          id: 1,
+          name: 1
+        },
+        {
+          id: 2,
+          name: 2
+        },
+        {
+          id: 3,
+          name: 3
+        }
+      ]
+    }
+  },
+  methods: {
+    clickOne (event) {
+      console.log(event.currentTarget)
+    },
+    clickTwo (event) {
+      console.log(event.currentTarget)
+    },
+    clickThree (event) {
+      console.log(event.currentTarget)
+    },
+    clickUl (event) {
+      console.log(event)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .home{
+        width: 100px;
+        height: 100px;
+        background: yellowgreen;
+    }
+    .one{
+        width: 60px;
+        height: 60px;
+        background: blueviolet;
+    }
+    .two{
+        width: 30px;
+        height: 30px;
+        background: darkorange;
+    }
+</style>
